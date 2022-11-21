30b3561439c4eba4f8fd9e4ff7be981561a68487
Octavio Lara <octavioasapchi@gmail.com>
2022-11-21 17:02:16 -0400

    [:wrench: fix weird error]

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -8,12 +8,16 @@ import {
   Button,
   Checkbox,
   Text,
+  Grid,
+  GridItem,
+  SimpleGrid,
   Heading
 } from '@chakra-ui/react';
 // Local components
 import Container from '@/components/atoms/Container';
 import { Logo } from '@/components/atoms/icons';
 import useAuth from '@/hooks/auth/';
+import CampImg from '@/assets/imgs/camp.jpeg';
 
 const Auth = () => {
   const navigate = useNavigate();
@@ -25,73 +29,71 @@ const Auth = () => {
   };
 
   return (
-    <Box bgColor="#fafafa">
-      <Container
-        h="100vh"
-        w="100vw"
-        display={'flex'}
-        justifyContent="center"
-        alignItems="center"
-        position="relative"
-      >
-        <Box position="absolute" top="10">
-          <Logo cursor="pointer" onClick={() => navigate('/')} />
-        </Box>
-        <Flex
-          bgColor="brand.white"
-          borderRadius="6px"
-          direction={'column'}
-          gap={5}
-          maxW="500px"
-          mt={9}
-          mx="auto"
-          p={8}
-          shadow="md"
-          w="90%"
-        >
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Flex gap={5} direction={'column'} w="100%">
-              <Heading as="h2" size="lg" color="brand.primary">
-                ¿Qué esperas para jugar?
-              </Heading>
-              <Input placeholder="Correo" {...register('email')} />
-              <Input
-                placeholder="Password"
-                type="password"
-                {...register('password')}
-              />
-              <Checkbox color="brand.200" fontWeight={'100'}>
-                Recuérdame
-              </Checkbox>
-              <Button
-                type="submit"
-                w="50%"
-                alignSelf={'center'}
-                variant="primary"
-                _hover={{ shadow: 'lg' }}
-              >
-                Iniciar Sesión
-              </Button>
-            </Flex>
-          </form>
-          <Box mt="3">
-            <Text fontSize={'14px'} textAlign="center" cursor="pointer">
-              ¿Miembro de un club{' '}
-              <Text as="span" color="brand.primary">
-                afiliado?
-              </Text>{' '}
-              Solicita tus datos
-            </Text>
-            <Text fontSize={'14px'} textAlign="center" mt="3" cursor="pointer">
-              ¿Deseas registrar tu club en{' '}
-              <Text as="span" color="brand.primary">
-                Golfincorp
-              </Text>
-              ?
-            </Text>
-          </Box>
-        </Flex>
-      </Container>
+    <Box bgColor="#fafafa" minH="100vh" minW="100vw">
+      <SimpleGrid columns={2} h="100vh">
+        <SimpleGrid>
+          <Box
+            backgroundImage={CampImg}
+            backgroundPosition="center"
+            backgroundSize={'cover'}
+            h="100%"
+          ></Box>
+        </SimpleGrid>
+        <GridItem placeItems={'center'}>
+          <Grid h="100%" p="10" placeContent={'center'}>
+            <Box bgColor="brand.white" borderRadius="10px" p={8} shadow="md">
+              <Logo cursor="pointer" onClick={() => navigate('/')} />
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Flex gap={5} direction={'column'} w="100%">
+                  <Heading as="h2" size="lg" color="brand.primary">
+                    ¿Qué esperas para jugar?
+                  </Heading>
+                  <Input placeholder="Correo" {...register('email')} />
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    {...register('password')}
+                  />
+                  <Checkbox color="brand.200" fontWeight={'100'}>
+                    Recuérdame
+                  </Checkbox>
+                  <Button
+                    type="submit"
+                    w="50%"
+                    alignSelf={'center'}
+                    variant="primary"
+                    _hover={{ shadow: 'lg' }}
+                  >
+                    Iniciar Sesión
+                  </Button>
+                </Flex>
+              </form>
+
+              <Box mt="3">
+                <Text fontSize={'14px'} textAlign="center" cursor="pointer">
+                  ¿Miembro de un club{' '}
+                  <Text as="span" color="brand.primary">
+                    afiliado?
+                  </Text>{' '}
+                  Solicita tus datos
+                </Text>
+                <Text
+                  fontSize={'14px'}
+                  textAlign="center"
+                  mt="3"
+                  cursor="pointer"
+                >
+                  ¿Deseas registrar tu club en{' '}
+                  <Text as="span" color="brand.primary">
+                    Golfincorp
+                  </Text>
+                  ?
+                </Text>
+              </Box>
+            </Box>
+          </Grid>
+        </GridItem>
+      </SimpleGrid>
     </Box>
   );
 };
