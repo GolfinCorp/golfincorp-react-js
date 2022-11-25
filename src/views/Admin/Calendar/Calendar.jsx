9030411bec2e47be75cdcc6d75d46503520dadee
Octavio Lara <octavioasapchi@gmail.com>
2022-11-25 17:42:16 -0400

    :sparkles: add calendar calculation

--- src/views/Admin/Calendar/Calendar.jsx
+++ src/views/Admin/Calendar/Calendar.jsx
@@ -4,6 +4,28 @@ import { HourSchedule } from '@/components/organisms';
 import { Divider, SimpleGrid, GridItem, Box } from '@chakra-ui/react';
 
 const Calendar = () => {
+  function getDaysInMonth(month, year) {
+    const date = new Date(year, month, 1);
+    let days = [];
+    while (date.getMonth() === month) {
+      days.push(new Date(date));
+      date.setDate(date.getDate() + 1);
+    }
+    let firstDay = days[0].getDay();
+    let finalDay = days[days.length - 1].getDay();
+    if (firstDay === 0) {
+      days = [...Array(6).fill(null), ...days];
+      return days;
+    }
+    days = [...Array(firstDay - 1).fill(null), ...days];
+    return days;
+  }
+  const currentDate = new Date();
+  const daysInMonth = getDaysInMonth(
+    currentDate.getMonth(),
+    currentDate.getFullYear()
+  );
+
   return (
     <>
       <DateFlex>
@@ -13,7 +35,17 @@ const Calendar = () => {
       <Divider border="2px" />
       <SimpleGrid my="5" columns={12} gap={1}>
         <GridItem colSpan={{ base: 12, md: 4 }}>
-          <Box p="5"></Box>
+          <Box p="5">
+            <SimpleGrid columns={7}>
+              {daysInMonth?.map((day, index) => {
+                return (
+                  <GridItem key={index} colSpan={1}>
+                    {day === null ? 'XD' : day.getDay()}
+                  </GridItem>
+                );
+              })}
+            </SimpleGrid>
+          </Box>
         </GridItem>
         <GridItem colSpan={{ base: 12, md: 8 }}>
           <HourSchedule />
