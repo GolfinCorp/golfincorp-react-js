104a3b37018eb2cfb93bdd974d50cb05f04e0eb8
Octavio Lara <octavioasapchi@gmail.com>
2022-11-29 12:41:58 -0400

    [:construction: & :lipstick: Refactor UI]

--- src/components/molecules/members/MemberDrawer/MemberDrawer.jsx
+++ src/components/molecules/members/MemberDrawer/MemberDrawer.jsx
@@ -1,31 +1,15 @@
-import { useState } from 'react';
-import {
-  DrawerBody,
-  DrawerFooter,
-  Button,
-  Box,
-  Divider,
-  Text,
-  Flex
-} from '@chakra-ui/react';
+import { DrawerBody, DrawerFooter, Box, Divider } from '@chakra-ui/react';
 import { useMembers } from '@/hooks';
 import Form from '../Form/Form';
 import UpdatePassword from '../UpdatePassword';
+import DeleteMember from '../MemberDelete/DeleteMember';
 const MemberForm = ({ onClose, id }) => {
-  const { updateMember, deleteMember } = useMembers();
-  const [isDeleting, setIsDeleting] = useState(false);
-
+  const { updateMember } = useMembers();
   // Event handlers
   const handleSubmit = async (member) => {
     event.preventDefault();
     const updateResponse = await updateMember(member, id);
     if (!updateResponse) return;
-    onClose();
-  };
-
-  const handleDelete = async () => {
-    const deleteResponse = await deleteMember(id);
-    if (!deleteResponse) return;
     onClose();
   };
 
@@ -38,24 +22,8 @@ const MemberForm = ({ onClose, id }) => {
           <UpdatePassword />
         </Box>
       </DrawerBody>
-
       <DrawerFooter>
-        {isDeleting ? (
-          <Flex flexDir="column" gap="2">
-            <Text>Esta seguro que desea borrar este miembro?</Text>
-            <Button variant="danger-outlined" mr={3} onClick={handleDelete}>
-              Eliminar
-            </Button>
-          </Flex>
-        ) : (
-          <Button
-            variant="danger"
-            mr={3}
-            onClick={() => setIsDeleting((prev) => !prev)}
-          >
-            Eliminar
-          </Button>
-        )}
+        <DeleteMember onClose={onClose} id={id} />
       </DrawerFooter>
     </>
   );
